f411f3601f121150375bf6c285853315b862f674
stanochka <krozzochka@gmail.com>
2021-06-26 20:53:52 +0300

    Ignore empty cells

--- script.js
+++ script.js
@@ -19,16 +19,20 @@ const makeGrid = (() => {
   };
 })()
 
-//initial numbers upon start game
+//initial numbers upon starting game
 const fillGrid = () => {
   startButton.removeEventListener('click', fillGrid);
   const divs = document.querySelectorAll('#container>div');
   const array = [...Array(54)].map(_ => Math.ceil(Math.random() * 9));
-  array.forEach((_, i) => {
-    divs[i].textContent = array[i];
-    //TODO: make id as 'row-col'
-    divs[i].id = i});
-  //while (array.length) matrix.push(array.splice(0, 9));
+  array.forEach((_, i) => { divs[i].textContent = array[i]; });
+  while (array.length) matrix.push(array.splice(0, 9));
+  let divIDs = [];
+  for (let r=0; r<matrix.length; r++) {
+    for (let c=0; c<matrix[r].length; c++) {
+      divIDs.push(`${r}-${c}`);
+    };
+  };
+  divIDs.forEach((_, i) => { divs[i].id = divIDs[i];});
   trackTime();
   trackProgress();
 }
@@ -127,7 +131,7 @@ const trackProgress = () => {
 
 const resetAll = () => {
   pointCounter = 0;
-  points.textContent = pointsCounter;
+  points.textContent = pointCounter;
   matrix.length = 0;
   chosen.length = 0;
   const divs = document.querySelectorAll('#container>div');
@@ -139,36 +143,62 @@ const checkChosen = () => {
   let x = chosen[0][0];
   let y = chosen[1][0];
   if (+x + +y === 10 || x === y) {
-    console.log('match!')
     return checkPosition();
   } else return false;
 }
-//TODO: check according to matrix
-//TODO: count vertical
-//TODO: ignore empty cells
+
 const checkPosition = () => {
-  let iX = chosen[0][1];
-  let iY = chosen[1][1];
-  if (iX > iY) {[iX, iY] = [iY, iX]};
-  let between = iY - iX;
-  if (between === 1) return true;
-  else {
-    return false;
+  let posX = [+chosen[0][1].match(/^\d+/)[0], +chosen[0][1].match(/\d+$/)[0]];
+  let posY = [+chosen[1][1].match(/^\d+/)[0], +chosen[1][1].match(/\d+$/)[0]];
+  //same row
+  if (posX[0] === posY[0] &&
+      matrix[posX[0]].slice(posX[1]+1, posY[1])
+                     .filter(el => el !== null)
+                     .length === 0) return true;
+  //same column
+  else if (posX[1] === posY[1]) {
+    let toCheck = [];
+    let start, end;
+    posX[0] < posY[0] ? (start = posX[0], end = posY[0]) : (start = posY[0], end = posX[0]);
+    for (let i=start+1; i<end; i++) { toCheck.push(matrix[i][posX[1]]); }
+    if (toCheck.filter(el => el !== null).length === 0) return true;
+    else return false;
   }
+  //next element
+  else if (posX[0]+1 === posY[0] || posX[0] === posY[0]+1) {
+    let toCheck = [];
+    let startRow, startCol, endRow, endCol;
+    posX[0] < posY[0] ?
+    (startRow = posX[0], startCol = posX[1], endRow = posY[0], endCol = posY[1]) :
+    (startRow = posY[0], startCol = posY[1], endRow = posX[0], endCol = posX[1]);
+    console.log([startRow, startCol]);
+    console.log([endRow, endCol]);
+    if (startCol < 8) toCheck = toCheck.concat(matrix[startRow].slice(startCol+1));
+    if (endCol > 0) toCheck = toCheck.concat(matrix[endRow].slice(0, endCol));
+    console.log(toCheck);
+    if (toCheck.filter(el => el !== null).length === 0) return true;
+    else return false;
+  }
+  else return false;
 }
 
 const removeItems = () => {
   const divs = document.querySelectorAll('#container>div');
   setTimeout(() => {
-    divs[chosen[0][1]].textContent = '';
-    divs[chosen[0][1]].style.background = '';
-    divs[chosen[0][1]].style.cursor = '';
-    divs[chosen[1][1]].textContent = '';
-    divs[chosen[1][1]].style.background = '';
-    divs[chosen[1][1]].style.cursor = '';
+    divs.forEach(div => {
+      if (div.id ===chosen[0][1] || div.id ===chosen[1][1]) {
+        div.textContent = '';
+        div.style.background = '';
+        div.style.cursor = '';
+      }
+    });
+    let posX = [+chosen[0][1].match(/^\d+/)[0], +chosen[0][1].match(/\d+$/)[0]];
+    let posY = [+chosen[1][1].match(/^\d+/)[0], +chosen[1][1].match(/\d+$/)[0]];
+    matrix[posX[0]].splice(posX[1], 1, null);
+    matrix[posY[0]].splice(posY[1], 1, null);
     chosen.length = 0;
+    //add deleting row if empty
   }, 500);
-  //TODO: update matrix
 }
 
 const addPoints = () => {
@@ -183,7 +213,7 @@ function stepListener() {
     if (chosen.length < 2) {
       chosen.push([div.textContent, div.id]);
       if (chosen.length === 2) {
-        if (checkChosen()) {
+        if (checkChosen() && chosen[0][1] !== chosen[1][1]) {
           removeItems();
           addPoints();
         } else {
