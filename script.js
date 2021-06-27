000303240348c04e8cf28bb583328d9dc4ca1b97
stanochka <krozzochka@gmail.com>
2021-06-27 19:48:29 +0300

    Test

--- script.js
+++ script.js
@@ -316,8 +316,3 @@ startButton.addEventListener('click', startGame);
 add.addEventListener('click', expandGrid);
 //hint.addEventListener('click', getHint);
 undo.addEventListener('click', undoStep);
-
-//not move window on mobile
-//function BlockMove(event) {
-//  event.preventDefault() ;
-//}
