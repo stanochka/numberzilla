00209294c9f331f734af0ad76adeba2a3276afc4
stanochka <krozzochka@gmail.com>
2021-06-27 21:00:23 +0300

    Prevent elastic scrolling for iphone

--- script.js
+++ script.js
@@ -316,3 +316,15 @@ startButton.addEventListener('click', startGame);
 add.addEventListener('click', expandGrid);
 //hint.addEventListener('click', getHint);
 undo.addEventListener('click', undoStep);
+
+//preven elastic scrolling for container on iPhone
+var scrollMinX = 0;
+var scrollMinY = 0;
+var scrollMaxX = document.body.scrollWidth - window.innerWidth;
+var scrollMaxY = document.body.scrollHeight - window.innerHeight;
+document.onscroll = function () {
+    if (window.scrollX <= scrollMinX) scrollTo(scrollMinX, window.scrollY);
+    if (window.scrollX >= scrollMaxX) scrollTo(scrollMaxX, window.scrollY);
+    if (window.scrollY <= scrollMinY) scrollTo(window.scrollX, scrollMinY);
+    if (window.scrollY >= scrollMaxY) scrollTo(window.scrollX, scrollMaxY);
+};
